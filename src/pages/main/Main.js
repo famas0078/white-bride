44eaf670634988389a7c861137402b4cdfb431b0
Yason <90974730+famas0078@users.noreply.github.com>
2023-12-02 18:33:35 +0300

    1

---
+++ src/pages/main/Main.js
@@ -0,0 +1,12 @@
+import Header from "../../components/header/Header";
+import classes from "./main.css";
+
+const Main = () =>{
+  return (
+    <div className={classes.wrapper}>
+      <Header />
+    </div>
+  );
+}
+
+export default Main;
